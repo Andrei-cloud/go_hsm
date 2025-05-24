70aac33ab173b2cd5a08d8a1d13a912ec7946ba4
Andrei-cloud <andrei.babikov@gmail.com>
2025-05-25 00:15:25 +0400

    feat: implement KQ command for ARQC verification and ARPC generation (Mode=0|1|2 Schema=0)

---
+++ internal/commands/plugins/KQ/gen.go
@@ -0,0 +1,2 @@
+//go:generate plugingen -cmd=KQ -logic=github.com/andrei-cloud/go_hsm/internal/hsm/logic -version=0.1.0 -desc "ARQC Verification and/or ARPC Generation" -author "Andrey Babikov" -out=.
+package main
